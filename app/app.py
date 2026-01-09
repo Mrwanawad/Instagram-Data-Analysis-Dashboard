fc72e5dcfd139fb5820a7f28b7c5129e2d817310
mrwanawad <mrwaanawad@gmail.com>
2026-01-09 20:29:12 +0200

    Finishing the dashboard

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 from PIL import Image
-from utils import human_readable, instagram_palette
+from utils import human_readable, instagram_palette, apply_figure_layout, months_order
 import os
 # streamlit run app/app.py
 
@@ -35,12 +36,87 @@ for kpi_box, text_plus_kpi in zip( [ kpi_box1, kpi_box2, kpi_box3, kpi_box4, kpi
 reach_by_content_category_df = df.groupby( 'content_category' )['reach'].sum().\
     sort_values( ascending=True ).to_frame().reset_index()
 
-chart_1, chart_2, chart_3 = st.columns( 3 )
+chart_1, chart_2, chart_3 = st.columns( [ 0.5, 0.4, 0.75 ] )
 
 chart_reach_by_content_category =  px.histogram( reach_by_content_category_df, x = 'reach', y = 'content_category', color = 'content_category',
              color_discrete_sequence= instagram_palette, template= 'simple_white', width = 600
-             )    
-chart_1.plotly_chart( chart_reach_by_content_category )
+             )
+chart_reach_by_content_category = apply_figure_layout( chart_reach_by_content_category, title_text= 'Reach By Content Category', xlabel_text= 'Total Reach'  , ylabel_text=' ' ).\
+                                  update_layout( showlegend = False )
+chart_1.plotly_chart( chart_reach_by_content_category, use_container_width= True )
 
-insta_img = Image.open( './Insta-logo.jpg' )
+insta_img = Image.open( './Insta-logo.jpg' ).resize( ( 350, 350 ) )
 chart_2.write( insta_img )
+
+reach_and_caption_len_by_hashs_df =   df.groupby( 'hashtags_count' )[ [ 'reach', 'caption_length' ] ].sum().reset_index()
+chart_reach_caption_by_hash_count = go.Figure()
+chart_reach_caption_by_hash_count.add_bar(
+    x = reach_and_caption_len_by_hashs_df['hashtags_count'],
+    y = reach_and_caption_len_by_hashs_df['reach'],
+    marker_color="#7A3FCB",
+    name = 'Reach by Hashtags Count'
+)
+chart_reach_caption_by_hash_count.add_scatter(
+    x = reach_and_caption_len_by_hashs_df['hashtags_count'],
+    y = reach_and_caption_len_by_hashs_df['caption_length'],
+    mode = "lines+markers",
+    yaxis= 'y2',
+    name = 'Caption Length by Hashtags Count'
+)
+chart_reach_caption_by_hash_count.update_layout(
+    title="Relation between reach and caption length by hashtags count",
+    xaxis_title="Hashtags count",
+
+    yaxis=dict(
+        title="Sum of reach",
+        tickformat=".2s"
+    ),
+
+    yaxis2=dict(
+        title=dict(
+                text = 'Sum of Caption Length', font = dict( family = 'times', size = 20, color = 'black' )
+            ),
+        overlaying="y",
+        side="right",
+        tickformat=".2s",
+        tickfont =dict( family = 'times', size = 15, color = 'black' )
+
+    ),
+    
+    template = 'simple_white', bargap = 0.2,
+    
+    legend = dict(
+        orientation = 'h', y = 1.25, x = 0.2,
+        font = dict( family = 'times', size = 18, color = 'black' )
+    )
+    
+)    
+chart_reach_caption_by_hash_count = apply_figure_layout( chart_reach_caption_by_hash_count )
+chart_3.plotly_chart( chart_reach_caption_by_hash_count, use_container_width=True )
+
+chart_21, chart_22 = st.columns( [0.3, 0.7] ) 
+
+chart_followers_gained_by_traffic = px.histogram(df, x = 'traffic_source', y='followers_gained', color = 'traffic_source',
+             color_discrete_sequence= instagram_palette[ 4 : 9 ], template = 'simple_white', text_auto=True,
+             )
+chart_followers_gained_by_traffic = apply_figure_layout( chart_followers_gained_by_traffic, ylabel_text= ' ', title_text = 'Total No. of Followers gained by Traffic Source' ).update_layout( showlegend = False )
+chart_followers_gained_by_traffic = chart_followers_gained_by_traffic.update_layout(title=dict( font = dict( size = 17 ) ) )
+chart_21.plotly_chart( chart_followers_gained_by_traffic, use_container_width= True )
+
+df['Month Name'] = pd.Categorical( values = df['Month Name'], categories= months_order, ordered=True)
+followers_gained_by_month_df = df.groupby( 'Month Name' )['followers_gained'].sum().to_frame().reset_index()
+
+chart_followers_gained_by_month = px.line( followers_gained_by_month_df, x = 'Month Name', y = 'followers_gained',
+        template = 'simple_white', markers = True, text='followers_gained', width = 900)
+chart_followers_gained_by_month.update_traces(
+    texttemplate="%{y:.2s}",
+    textposition="top center",
+    line=dict(width=5, color = '#E1306C'),
+    textfont=dict(
+        family="Montserrat",
+        size=20,
+        color="black"
+    )
+)
+chart_followers_gained_by_month = apply_figure_layout(  chart_followers_gained_by_month, ylabel_text=' ', title_text= 'Total Followers Gained by Month' ).update_layout(title=dict(font =dict( size = 17 )))
+chart_22.plotly_chart( chart_followers_gained_by_month, use_container_width=True )
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,7 @@
+import plotly.express as px
+from plotly.graph_objects import Figure
+
+
 def human_readable(n):
     if n >= 1_000_000_000:
         return f"{n/1_000_000_000:.0f}Bn"
@@ -19,4 +23,42 @@ instagram_palette = [
     "#F56040",  # Orange
     "#FCAF45",  # Light Orange
     "#FFD166"   # Yellow
-]    
+]
+
+months_order = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+def apply_figure_layout( fig : Figure, title_text = '', xlabel_text = '', ylabel_text = '' ) -> Figure :
+    
+    xlabel_text = fig.layout.xaxis.title.text if xlabel_text == '' else xlabel_text
+    ylabel_text = fig.layout.yaxis.title.text if ylabel_text == '' else ylabel_text
+    title_text = fig.layout.title.text if ylabel_text == '' else title_text
+    
+    fig.update_layout(
+        
+        title = dict(
+            text = title_text,
+            font = dict( family = 'times', size = 25, color = 'black' )
+        ),
+        
+        xaxis = dict(
+            title = dict(
+                text = xlabel_text, font = dict( family = 'times', size = 20, color = 'black' )
+            ),
+        tickfont =dict( family = 'times', size = 15, color = 'black' )
+
+        ),
+        
+        yaxis = dict(
+            title = dict(
+                text = ylabel_text, font = dict( family = 'times', size = 20, color = 'black' )
+            ),
+        tickfont =dict( family = 'times', size = 15, color = 'black' )
+
+        ),
+    )  
+    
+    return fig
+
